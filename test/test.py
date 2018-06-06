97bc1a932add6ca6d81bb5dd3ea49a5d3cdc2e23
page2me kitarotao <page2me@msn.com>
2018-06-06 10:43:26 +0700

    test commit

--- test/test.py
+++ test/test.py
@@ -1,11 +1,4 @@
 print("Hello World!")
 print("Hello World!")
 print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+
